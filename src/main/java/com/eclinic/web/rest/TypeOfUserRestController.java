f6fe9dae2dad12111ea1407155f2655a2d08c50f
shandrya <youkea@gmail.com>
2015-05-26 11:00:47 +0200

    Update TypeOfUserRestController.java

--- src/main/java/com/eclinic/web/rest/TypeOfUserRestController.java
+++ src/main/java/com/eclinic/web/rest/TypeOfUserRestController.java
@@ -5,32 +5,33 @@ import com.eclinic.dao.TypeOfUserDAO;
 
 import com.eclinic.domain.Permission;
 import com.eclinic.domain.TypeOfUser;
-
 import com.eclinic.service.TypeOfUserService;
 
-import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.stereotype.Controller;
-
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.WebDataBinder;
-
 import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 /**
  * Spring Rest controller that handles CRUD requests for TypeOfUser entities
  * 
  */
-
-@Controller("TypeOfUserRestController")
+@Path("/TypeOfUser")
+@Component("TypeOfUserRestController")
 public class TypeOfUserRestController {
 
 	/**
@@ -54,27 +55,10 @@ public class TypeOfUserRestController {
 	@Autowired
 	private TypeOfUserService typeOfUserService;
 
-	/**
-	 * Delete an existing TypeOfUser entity
-	 * 
-	 */
-	@RequestMapping(value = "/TypeOfUser/{typeofuser_id}", method = RequestMethod.DELETE)
-	@ResponseBody
-	public void deleteTypeOfUser(@PathVariable Integer typeofuser_id) {
-		TypeOfUser typeofuser = typeOfUserDAO.findTypeOfUserByPrimaryKey(typeofuser_id);
-		typeOfUserService.deleteTypeOfUser(typeofuser);
-	}
-
-	/**
-	 * Select an existing TypeOfUser entity
-	 * 
-	 */
-	@RequestMapping(value = "/TypeOfUser/{typeofuser_id}", method = RequestMethod.GET)
-	@ResponseBody
-	public TypeOfUser loadTypeOfUser(@PathVariable Integer typeofuser_id) {
-		return typeOfUserDAO.findTypeOfUserByPrimaryKey(typeofuser_id);
-	}
-
+	
+	
+	public TypeOfUserRestController(){}
+	
 	/**
 	 * Register custom, context-specific property editors
 	 * 
@@ -92,90 +76,147 @@ public class TypeOfUserRestController {
 		binder.registerCustomEditor(Long.class, new org.skyway.spring.util.databinding.NaNHandlingNumberEditor(Long.class, true));
 		binder.registerCustomEditor(Double.class, new org.skyway.spring.util.databinding.NaNHandlingNumberEditor(Double.class, true));
 	}
+	
+	/**
+	 * Delete an existing TypeOfUser entity
+	 * 
+	 */
+
+	
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{typeofuser_id}")
+	@DELETE
+	public void deleteTypeOfUser(@PathParam("typeofuser_id") Integer typeofuser_id) {
+		TypeOfUser typeofuser = typeOfUserDAO.findTypeOfUserByPrimaryKey(typeofuser_id);
+		typeOfUserService.deleteTypeOfUser(typeofuser);
+	}
+
+	/**
+	 * Select an existing TypeOfUser entity
+	 * 
+	 */
+
+	
+	@GET
+	@Path("/{typeofuser_id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response loadTypeOfUser(@PathParam("typeofuser_id") Integer typeofuser_id) {
+		return Response.ok(typeOfUserDAO.findTypeOfUserByPrimaryKey(typeofuser_id)).build();
+	}
+
+	
 
 	/**
 	 * Create a new TypeOfUser entity
 	 * 
 	 */
-	@RequestMapping(value = "/TypeOfUser", method = RequestMethod.POST)
-	@ResponseBody
-	public TypeOfUser newTypeOfUser(@RequestBody TypeOfUser typeofuser) {
+
+	
+	@POST
+	@Path("/new")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response newTypeOfUser( TypeOfUser typeofuser) {
 		typeOfUserService.saveTypeOfUser(typeofuser);
-		return typeOfUserDAO.findTypeOfUserByPrimaryKey(typeofuser.getId());
+		return Response.ok(typeOfUserDAO.findTypeOfUserByPrimaryKey(typeofuser.getId())).build();
 	}
 
 	/**
 	 * Delete an existing Permission entity
 	 * 
 	 */
-	@RequestMapping(value = "/TypeOfUser/{typeofuser_id}/permission/{permission_id}", method = RequestMethod.DELETE)
-	@ResponseBody
-	public void deleteTypeOfUserPermission(@PathVariable Integer typeofuser_id, @PathVariable Integer related_permission_id) {
-		typeOfUserService.deleteTypeOfUserPermission(typeofuser_id, related_permission_id);
+
+	
+	@DELETE
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{typeofuser_id}/permission/{permission_id}")
+	public Response deleteTypeOfUserPermission(@PathParam("typeofuser_id") Integer typeofuser_id,
+			@PathParam("related_permission_id") Integer related_permission_id) {
+		return Response.ok(typeOfUserService.deleteTypeOfUserPermission(typeofuser_id, related_permission_id)).build();
 	}
 
 	/**
 	 * Create a new Permission entity
 	 * 
 	 */
-	@RequestMapping(value = "/TypeOfUser/{typeofuser_id}/permission", method = RequestMethod.POST)
-	@ResponseBody
-	public Permission newTypeOfUserPermission(@PathVariable Integer typeofuser_id, @RequestBody Permission permission) {
+
+	
+	@Path("/{typeofuser_id}/permission")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response newTypeOfUserPermission(@PathParam("typeofuser_id") Integer typeofuser_id,
+			Permission permission) {
 		typeOfUserService.saveTypeOfUserPermission(typeofuser_id, permission);
-		return permissionDAO.findPermissionByPrimaryKey(permission.getId());
+		return Response.ok(permissionDAO.findPermissionByPrimaryKey(permission.getId())).build();
 	}
 
 	/**
 	 * Save an existing Permission entity
 	 * 
 	 */
-	@RequestMapping(value = "/TypeOfUser/{typeofuser_id}/permission", method = RequestMethod.PUT)
-	@ResponseBody
-	public Permission saveTypeOfUserPermission(@PathVariable Integer typeofuser_id, @RequestBody Permission permission) {
+
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{typeofuser_id}/permission")
+	@PUT
+	public Response saveTypeOfUserPermission(@PathParam("typeofuser_id") Integer typeofuser_id,
+			Permission permission) {
 		typeOfUserService.saveTypeOfUserPermission(typeofuser_id, permission);
-		return permissionDAO.findPermissionByPrimaryKey(permission.getId());
+		return Response.ok(permissionDAO.findPermissionByPrimaryKey(permission.getId())).build();
 	}
 
 	/**
 	 * Show all TypeOfUser entities
 	 * 
 	 */
-	@RequestMapping(value = "/TypeOfUser", method = RequestMethod.GET)
-	@ResponseBody
-	public List<TypeOfUser> listTypeOfUsers() {
-		return new java.util.ArrayList<TypeOfUser>(typeOfUserService.loadTypeOfUsers());
+
+	
+	@GET
+	@Path("/list")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response listTypeOfUsers() {
+		return  Response.ok(typeOfUserService.loadTypeOfUsers()).build();
 	}
 
 	/**
 	 * View an existing Permission entity
 	 * 
 	 */
-	@RequestMapping(value = "/TypeOfUser/{typeofuser_id}/permission/{permission_id}", method = RequestMethod.GET)
-	@ResponseBody
-	public Permission loadTypeOfUserPermission(@PathVariable Integer typeofuser_id, @PathVariable Integer related_permission_id) {
+
+	
+	@GET
+	@Path("/{typeofuser_id}/permission/{permission_id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response loadTypeOfUserPermission(@PathParam("typeofuser_id") Integer typeofuser_id,
+			@PathParam("related_permission_id") Integer related_permission_id) {
 		Permission permission = permissionDAO.findPermissionByPrimaryKey(related_permission_id, -1, -1);
 
-		return permission;
+		return Response.ok(permission).build();
 	}
 
 	/**
 	 * Get Permission entity by TypeOfUser
 	 * 
 	 */
-	@RequestMapping(value = "/TypeOfUser/{typeofuser_id}/permission", method = RequestMethod.GET)
-	@ResponseBody
-	public Permission getTypeOfUserPermission(@PathVariable Integer typeofuser_id) {
-		return typeOfUserDAO.findTypeOfUserByPrimaryKey(typeofuser_id).getPermission();
+
+	@GET
+	@Path("/{typeofuser_id}/permission")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getTypeOfUserPermission(@PathParam("typeofuser_id") Integer typeofuser_id) {
+		return Response.ok(typeOfUserDAO.findTypeOfUserByPrimaryKey(typeofuser_id).getPermission()).build();
 	}
 
 	/**
 	 * Save an existing TypeOfUser entity
 	 * 
 	 */
-	@RequestMapping(value = "/TypeOfUser", method = RequestMethod.PUT)
-	@ResponseBody
-	public TypeOfUser saveTypeOfUser(@RequestBody TypeOfUser typeofuser) {
+
+	
+	@PUT
+	@Path("/save")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response saveTypeOfUser(TypeOfUser typeofuser) {
 		typeOfUserService.saveTypeOfUser(typeofuser);
-		return typeOfUserDAO.findTypeOfUserByPrimaryKey(typeofuser.getId());
+		return Response.ok(typeOfUserDAO.findTypeOfUserByPrimaryKey(typeofuser.getId())).build();
 	}
 }
