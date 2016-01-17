ee1c0228b952712d1d93a2f9e7971de80e304f05
Adrian-java <arembisz19@gmail.com>
2016-01-17 15:36:51 +0100

    VisitEdit

---
+++ ClinicMain/src/main/java/com/eclinic/model/VisitEdit.java
@@ -0,0 +1,25 @@
+package com.eclinic.model;
+
+public class VisitEdit {
+
+	private Integer visitId;
+	
+	private String desciption;
+
+	public Integer getVisitId() {
+		return visitId;
+	}
+
+	public void setVisitId(Integer visitId) {
+		this.visitId = visitId;
+	}
+
+	public String getDesciption() {
+		return desciption;
+	}
+
+	public void setDesciption(String desciption) {
+		this.desciption = desciption;
+	}
+	
+}
